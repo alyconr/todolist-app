fe74f64d64f10c714391a60212ae89eed41a577e
alyconr <alyconr@hotmail.com>
2023-06-17 17:29:25 -0500

    :wrench:  Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import TaskManager from './modules/TaskManager.js';
+import './assets/images/icons8-cancel-48.png';
+import './assets/images/icons8-save-48.png';
+import './assets/images/icons8-botón-de-radio-sin-marcar-48.png';
+import './assets/images/icons8-ok-48.png';
+import './assets/images/icons8-editar-48.png';
+import './assets/images/icons8-borrar-para-siempre-48.png';
 // eslint-disable-next-line no-unused-vars
 const taskManager = new TaskManager();
